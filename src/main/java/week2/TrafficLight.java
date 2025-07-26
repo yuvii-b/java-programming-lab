5477d5f52ee829494725df51a9b3d4fc1b62b399
yuvaraj <yuvaraj.codes@gmail.com>
2025-07-26 19:59:08 +0530

    w2q7

---
+++ src/main/java/week2/TrafficLight.java
@@ -0,0 +1,22 @@
+// Question 7 - Traffic Light Response
+package week2;
+
+import java.util.Scanner;
+
+// Question 7
+public class TrafficLight {
+    public static void main(String[] args) {
+        System.out.println("Name: Yuvaraj B(2024503541) | Week2 | Question7 - Traffic Light Response\n");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the traffic light colour (red, yellow, green): ");
+        String colour = sc.nextLine().toLowerCase(); // converted to lowercase
+
+        switch (colour) {
+            case "red" -> System.out.println("Stop");
+            case "yellow" -> System.out.println("Ready to move");
+            case "green" -> System.out.println("Go");
+            default -> System.out.println("Invalid colour");
+        }
+    }
+}
