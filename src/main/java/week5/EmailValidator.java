7088d7d864752a926bb39c349b9391bf0143ef1e
yuvaraj <yuvaraj.codes@gmail.com>
2025-08-14 20:15:38 +0530

    w5q7

---
+++ src/main/java/week5/EmailValidator.java
@@ -0,0 +1,46 @@
+package week5;
+
+import java.util.Scanner;
+
+public class EmailValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter email: ");
+        String email = sc.nextLine();
+
+        boolean isValid = true;
+
+        if (email.indexOf('@') == -1 || email.indexOf('@') != email.lastIndexOf('@')) {
+            isValid = false;
+        } else {
+            String[] parts = email.split("@");
+            if (parts.length != 2) {
+                isValid = false;
+            } else {
+                String username = parts[0];
+                String domain = parts[1];
+
+                if (username.isEmpty() || username.length() > 25) {
+                    isValid = false;
+                } else if (!username.matches("[a-zA-Z0-9_\\-+]+")) {
+                    isValid = false;
+                }
+
+                if (!(domain.endsWith(".com") || domain.endsWith(".in") || domain.endsWith(".edu"))) {
+                    isValid = false;
+                }
+                if (isValid) {
+                    System.out.println("Valid Email");
+                    System.out.println("Username: " + username);
+                    System.out.println("Domain: " + domain);
+                }
+            }
+        }
+
+        if (!isValid) {
+            System.out.println("Invalid Email");
+        }
+    }
+}
+
