e56fc9275fece01354aa7f0630c54edf85c886b9
yuvaraj <yuvaraj.codes@gmail.com>
2025-08-14 19:48:42 +0530

    w5q1

---
+++ src/main/java/week5/StringReverse.java
@@ -0,0 +1,28 @@
+package week5;
+
+import java.util.Scanner;
+
+public class StringReverse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        char[] chars = input.toCharArray();
+        int left = 0;
+        int right = chars.length - 1;
+
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+
+        String reversed = new String(chars);
+        System.out.println("Reversed string: " + reversed);
+    }
+}
+
